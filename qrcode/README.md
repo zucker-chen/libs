1c3de9f0c2bb4af2991056ca01b9008428fc75e0
zucker.chen <timeontheway@163.com>
2017-05-09 12:59:07 +0800

    add zbar lib compile.

---
+++ qrcode/README.md
@@ -0,0 +1,16 @@
+
+# zbar
+二维码生成解析工具库
+
+## 下载安装编译方法
+`sh build-zbar.sh`  
+默认pc编译，如果要交叉编译则修改build-zbar.sh  
+`enable_cross_compile="enable"`  
+
+## 使用方法
+```
+略
+```
+
+## 依赖库
+`libiconv`
--- qrcode/build-zbar.sh
+++ qrcode/build-zbar.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 zbar_ver="zbar-0.10"
-enable_cross_compile="enable"	# enable/disable
+libiconv_ver="libiconv-1.14"
+enable_cross_compile="disable"	# enable/disable
 cross_prefix="arm-fullhan-linux-uclibcgnueabi-"
-output_path="$(pwd)/$zbar_ver/build"
+libiconv_output_path="$(pwd)/$libiconv_ver/build"
+zbar_output_path="$(pwd)/$zbar_ver/build"
 
+# Fetch Sources
+if [ ! -d $libiconv_ver ]; then
+	mkdir $libiconv_ver
+	wget http://ftp.gnu.org/pub/gnu/libiconv/${libiconv_ver}.tar.gz
+	tar xf ${libiconv_ver}.tar.gz
+fi
 # Fetch Sources
 if [ ! -d $zbar_ver ]; then
 	mkdir $zbar_ver
@@ -17,14 +25,34 @@ if [ "$enable_cross_compile" = "enable" ]; then
 	cross_pri_cflags="--host=arm-linux CC=${cross_prefix}gcc CPP=${cross_prefix}cpp CXX=${cross_prefix}g++"
 fi
 
+# Compile libiconv
+cd $libiconv_ver
+# ./configure
+pri_cflags="$cross_pri_cflags --prefix=$libiconv_output_path --enable-static"
+sh configure $pri_cflags	# ;echo "sh configure $pri_cflags"
+
+# make & install
+make -j4 && make install
+cd -
+
+# Compile zbar
 cd $zbar_ver
+# "./libtool: eval: line 961: syntax error near unexpected token `|'"
+export NM=nm
 
 # ./configure
-pri_cflags="$cross_pri_cflags --prefix=$output_path --enable-static
-			--without-imagemagick --without-gtk --without-qt --without-python --with-x --disable-video
-			--without-libiconv-prefix --without-jpeg --without-xv --without-xshm"
+pri_cflags="$cross_pri_cflags --prefix=$zbar_output_path --enable-static
+			--without-imagemagick --without-gtk --without-qt --without-python --disable-video
+			--without-jpeg --without-xv --without-xshm --without-x
+			CPPFLAGS=-I$libiconv_output_path/include LDFLAGS=-L$libiconv_output_path/lib"
 sh configure $pri_cflags	# ;echo "sh configure $pri_cflags"
 
 # make & install
 make -j4 && make install
 
+
+
+# Note:
+# libiconv must profile ==> "zbar/qrcode/qrdectxt.c:9:19: error: iconv.h: No such file or directory"
+# libiconv-1.15 ==> "getprogname.c:147:4: error: #error "getprogname module not ported to this OS""
+# 

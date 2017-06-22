df64c9da3793398e2ce0b7d708fe8af35607d64e
zucker.chen <timeontheway@163.com>
2017-06-22 09:21:09 +0800

    fixed bug for uzip jffs2 with loop mode, because erasesize incorrect.

--- scripts/unjffs2.sh
+++ scripts/unjffs2.sh
@@ -8,40 +8,49 @@
 
 sh_args=$@
 mtd_size=16384	# K
-img_file=rootfs.jffs2 # jffs2...
-img_type=jffs2
+img_file=rootfs.jffs2   # jffs2...
+img_type=jffs2          # file -b rootfs.jffs2 | cut -d ' ' -f2
+uzip_mode=mtdram
 mnt_dir=./mnt
 out_dir=./out
 
 
 usage()
 {
-    printf "Usage: %s [-t <arg>] [-s <arg>] [-o <arg>] [-h] <file>\n" "$(basename "$0")"
+    printf "Usage: %s [-t <type>] [-m <mode>]  [-o <dir>] [-s] [-h] <file>\n" "$(basename "$0")"
     printf "\n"
     printf "Options:\n"
-    printf "    -t <arg>  arg is image type(jffs2...), default jffs2\n"
-    printf "    -s <arg>  arg is mtd total size(K), default 16MB\n"
-    printf "    -o <arg>  output dir name, default ./out\n"
+    printf "    file      input image file, like: rootfs.jffs2\n"
+    printf "    -t <type> image type(jffs2...), default jffs2\n"
+    printf "    -m <mode> uzip mode(mtdram or loop), default mtdram, tips: mtdram need enough ram capacity\n"
+    printf "    -o <dir>  output dir name, default ./out\n"
+    printf "    -s        assign use file size to mtd total size(K), default 16MB\n"
     printf "    -h        print usage and exit\n"
-    printf "    -v        verbose logging\n"
 }
 
 opts_parse()
 {
-    while getopts :t:s:o::h option; do
+	local s_parm=0
+
+    while getopts :t:m:o::sh option;
+	do
+		
         case "${option}" in
             t)
                 img_type="${OPTARG}"
                 ;;
 
             s)
-                mtd_size="${OPTARG}"
+				s_parm=1
+                ;;
+
+            m)
+                uzip_mode="${OPTARG}"
                 ;;
 
             o)
                 out_dir="${OPTARG}"
                 ;;
-
             h)
                 usage
                 exit 0
@@ -73,15 +82,16 @@ opts_parse()
     fi
 
     img_file="$1"
+	[ $s_parm -eq 1 ] && mtd_size=$(du -bk $img_file | awk '{print $1}')
 	
-	echo "
+	echo "\
 sh_args=$sh_args
 mtd_size=$mtd_size
 img_file=$img_file
 img_type=$img_type
 mnt_dir=$mnt_dir
-out_dir=$out_dir
-	"
+out_dir=$out_dir\
+	" 1>/dev/null 2>&1
 }
 
 
@@ -137,13 +147,13 @@ loop_mount()
 	losetup $loop $img_file
 	
 	# convert it to a mtd device
-	modprobe block2mtd block2mtd=$loop,131072	# 131072 = erasesize 4K
+	modprobe block2mtd block2mtd=$loop,65536	# 65536 = erasesize 64K, must equal erasesize of 'mkfs.jffs2 -e'
 
 	# modprobe mtdblock, create /dev/mtdblock0
 	modprobe mtdblock
 
 	# modprobe jffs2, support mount -t auto ...
-	modprobe jffs2
+	#modprobe jffs2
 	
 	# mount
 	[ ! -d $mnt_dir ] && mkdir $mnt_dir
@@ -167,14 +177,14 @@ loop_unmount()
 
 mtdram_mount()
 {
-	# modprobe mtdram, with mtd_size(default 4MB), create /proc/mtd
+	# modprobe mtdram, with mtd_size(unit KB), create /proc/mtd
 	modprobe mtdram total_size=$mtd_size
 
 	# modprobe mtdchar
 	modprobe mtdchar
 	
 	# write the image to /dev/mtd0
-	dd if=$img_file of=/dev/mtd0
+	dd if=$img_file of=/dev/mtd0 1>/dev/null 2>&1
 	
 	# modprobe mtdblock, create /dev/mtdblock0
 	modprobe mtdblock
@@ -202,25 +212,30 @@ mtdram_umount()
 main()
 {
 	opts_parse $sh_args
-	
+
 	root_check
 	[ $? -ne 0 ] && exit 0		# exit current shell if no permission
-	
-	# loop_mount mount error, need fixed
-	#loop_mount
-	
-	#loop_unmount
-		
-	mtdram_mount
-	
-	mtdram_umount
 
+
+	if [ $uzip_mode = "mtdram" ]; then
+		mtdram_mount
+		mtdram_umount
+		echo "<mtdram>: uzip $img_type file $img_file done, output to $out_dir."
+	elif [ $uzip_mode = "loop" ]; then
+		loop_mount
+		loop_unmount
+		echo "<loop>: uzip $img_type file $img_file done, output to $out_dir."
+	else
+		echo "Error uzip mode input, only support 'mtdram' or 'loop'"
+	fi
 }
 
 main
 
 
 
-
+# tips:
+# mtdram need enough ram capacity.
+# loop need assign erasesize, must equal the erasesize of 'mkfs.jffs2 -e'
 
 

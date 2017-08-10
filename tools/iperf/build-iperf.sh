278464da60fc4b2b397b1c7fbce10ab2c6f0cceb
zucker.chen <timeontheway@163.com>
2017-08-10 20:03:51 +0800

    add iperf tool method.

---
+++ tools/iperf/build-iperf.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+iperf_ver="iperf-2.0.4-RELEASE"
+enable_cross_compile="disable"	# enable/disable
+cross_prefix="arm-hisiv500-linux-"
+output_path="$(pwd)/$iperf_ver/build"
+
+
+# Fetch Sources
+if [ ! -d $iperf_ver ]; then
+	mkdir $iperf_ver
+	wget https://github.com/esnet/iperf/archive/${iperf_ver#*iperf-}.tar.gz
+	tar xf ${iperf_ver}.tar.gz
+fi
+
+# Cross compile cflags
+if [ "$enable_cross_compile" = "enable" ]; then
+	cross_pri_cflags="--host=arm-linux CC=${cross_prefix}gcc CPP=${cross_prefix}cpp CXX=${cross_prefix}g++"
+fi
+
+cd $iperf_ver
+
+# ./configure
+pri_cflags="$cross_pri_cflags --prefix=$output_path"
+sh configure $pri_cflags
+
+# comment out #define malloc rpl_malloc, when make error.
+sed -i "s/^#define malloc rpl_malloc/\/\*#define malloc rpl_malloc\*\//" config.h
+
+# make & install
+make && make install
+
+
+# Note:

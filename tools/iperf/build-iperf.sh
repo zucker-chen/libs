79aeca34e7415caa7a46ecc34eb16ae9cf10c828
zucker.chen <timeontheway@163.com>
2018-04-23 20:51:31 -0700

    fixed build-iperf.sh

--- tools/iperf/build-iperf.sh
+++ tools/iperf/build-iperf.sh
@@ -4,12 +4,12 @@ iperf_ver="iperf-2.0.4-RELEASE"
 enable_cross_compile="disable"	# enable/disable
 cross_prefix="arm-hisiv500-linux-"
 output_path="$(pwd)/$iperf_ver/build"
-
+shell -e
 
 # Fetch Sources
 if [ ! -d $iperf_ver ]; then
 	mkdir $iperf_ver
-	wget https://github.com/esnet/iperf/archive/${iperf_ver#*iperf-}.tar.gz
+	wget https://github.com/esnet/iperf/archive/${iperf_ver#*iperf-}.tar.gz -O ${iperf_ver}.tar.gz
 	tar xf ${iperf_ver}.tar.gz
 fi
 
@@ -32,3 +32,5 @@ make && make install
 
 
 # Note:
+# ${iperf_ver#*iperf-}, cut off 'iperf-' from $iperf_ver
+# shell -e, exit shell if any cmd return not 0

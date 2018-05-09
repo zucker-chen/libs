749a0df57d170bdc87f1ba40824bb5816468b0c1
zucker.chen <timeontheway@163.com>
2018-05-09 00:15:46 -0700

    fixed memtester tool build sh.

--- tools/memtester/build-memtester.sh
+++ tools/memtester/build-memtester.sh
@@ -2,14 +2,14 @@
 
 memtester_ver="memtester-4.3.0"
 enable_cross_compile="disable"	# enable/disable
-cross_prefix="arm-fullhan-linux-uclibcgnueabi-"
+cross_prefix="arm-hisiv500-linux-"
 output_path="$(pwd)/$memtester_ver/build"
 
 # Fetch Sources
 if [ ! -d $memtester_ver ]; then
 	mkdir $memtester_ver
 	wget http://pyropus.ca/software/memtester/old-versions/memtester-4.3.0.tar.gz
-	tar xf ${memtester_ver}.tar.bz2
+	tar xvf ${memtester_ver}.tar.gz
 fi
 
 # Cross compile cflags
@@ -17,16 +17,22 @@ if [ "$enable_cross_compile" = "enable" ]; then
 	cross_pri_cflags="${cross_prefix}"
 fi
 
-sed -i "s/^.*cc /${cross_pri_cflags}cc /" ./$memtester_ver/conf-cc
-sed -i "s/^.*cc /${cross_pri_cflags}cc /" ./$memtester_ver/conf-ld
+sed -i "s/^.*cc /${cross_pri_cflags}gcc /" ./$memtester_ver/conf-cc
+sed -i "s/^.*cc /${cross_pri_cflags}gcc /" ./$memtester_ver/conf-ld
 
 # fix Makefile
 esc_path=$(echo $output_path | sed '/\//s/\//\\\//g')	# 全路径'/' 转义 '\/'
 #sed -n "s/^INSTALLPATH\t= .*$/INSTALLPATH\t= $esc_path/p" ./$memtester_ver/Makefile		# 整行替换 - 测试打印
 sed -i "s/^INSTALLPATH\t= .*$/INSTALLPATH\t= $esc_path/" ./$memtester_ver/Makefile		# 整行替换
+#sed -n "s/{bin,man\/man8}/bin \$(INSTALLPATH)\/man\/man8/p" ./$memtester_ver/Makefile	# 将"{bin,man/man8}"替换成"$(INSTALLPATH)/man/man8" - 测试打印
+sed -i "s/{bin,man\/man8}/bin \$(INSTALLPATH)\/man\/man8/" ./$memtester_ver/Makefile	# 将"{bin,man/man8}"替换成"$(INSTALLPATH)/man
+    /man8"
 
 cd $memtester_ver
 
 # make & install
 make -j4 && make install
 
+
+# tips
+# Makefile 中 mkdir -p {bin,man/man8} 语法解析会存在问题，不能正确创建目录
